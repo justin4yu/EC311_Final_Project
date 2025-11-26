e12340e6ca88c5e98dccf790984fa911a912c148
Justin Y <justinyu@bu.edu>
2025-11-25 20:24:15 -0500

    Added 3 states and case statement transitions, no actual implementation yet

--- game_fsm.v
+++ game_fsm.v
@@ -11,14 +11,14 @@ module game_fsm #(parameter game_timer = 30)(
    // States definition
     localparam IDLE    = 2'd0;
     localparam RUNNING = 2'd1;
-    localparam DONE    = 2'd2;
+    localparam FINISH  = 2'd2;
 
     reg [1:0] current_state, next_state;
 
     // State transitions
     always @(*) begin
        next_state = current_state;
-       case (current_state)
+       case (current_state) // Game can only transition in the following ways unless reset has been pressed
             IDLE: begin
                  if (startGame) begin
                     next_state = RUNNING;
@@ -26,16 +26,16 @@ module game_fsm #(parameter game_timer = 30)(
             end
             RUNNING: begin
                  if (timer_expired) begin
-                    next_state = DONE;
+                    next_state = FINISH;
                  end
             end
-            DONE: begin
+            FINISH: begin
                  if (reset) begin
                     next_state = IDLE;
                  end
             end
 
-            default: next_state = IDLE;
+            default: next_state = IDLE; // At reset, always start with IDLE state requiring user start input
         endcase
     end
 
