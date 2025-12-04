bdcbf9547f43f0cfe60d918fd90d08e7bf68c949
Justin Y <justinyu@bu.edu>
2025-12-03 21:32:42 -0500

    added game_active input

--- score_counter.v
+++ score_counter.v
@@ -1,6 +1,7 @@
 module score_counter (
     input            clkIn,          // 100MHz FPGA clock input
     input            reset,          // Still active low reset
+    input            game_active,    // To indicate if the game is running
     input            timer_expired,  // To signal score reset at start of a new game
     input            player_scored,  // Signal when player scores
     output reg [5:0] score           // (2^6)-1 score range (0-63) ** can revise if needed if we increase game timer range
