ce0879f972f82524955942272d8a14b0594572eb
Justin Y <justinyu@bu.edu>
2025-12-03 20:49:13 -0500

    Got rid of gameStart

--- score_counter.v
+++ score_counter.v
@@ -1,7 +1,6 @@
 module score_counter (
     input            clkIn,          // 100MHz FPGA clock input
     input            reset,          // Still active low reset
-    input            gameStart,      // Corner case to account for score increment bug even after starting a new game
     input            timer_expired,  // To signal score reset at start of a new game
     input            player_scored,  // Signal when player scores
     output reg [5:0] score           // (2^6)-1 score range (0-63) ** can revise if needed if we increase game timer range
